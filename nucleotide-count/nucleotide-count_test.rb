6de49b88f6a645bcd73ce4ca1afdd9589a47c4d4
Dan S Turner <turner@nowwithmoredan.com>
2013-11-06 10:43:07 -0800

    Finished nucleotide-count exercise

--- nucleotide-count/nucleotide-count_test.rb
+++ nucleotide-count/nucleotide-count_test.rb
@@ -12,35 +12,29 @@ class DNATest < MiniTest::Unit::TestCase
   end
 
   def test_repetitive_cytidine_gets_counted
-    skip
     assert_equal 5, DNA.new('CCCCC').count('C')
   end
 
   def test_repetitive_sequence_has_only_guanosine
-    skip
     expected = {'A' => 0, 'T' => 0, 'C' => 0, 'G' => 8}
     assert_equal expected, DNA.new('GGGGGGGG').nucleotide_counts
   end
 
   def test_counts_only_thymidine
-    skip
     assert_equal 1, DNA.new('GGGGGTAACCCGG').count('T')
   end
 
   def test_counts_a_nucleotide_only_once
-    skip
     dna = DNA.new('CGATTGGG')
     dna.count('T')
     assert_equal 2, dna.count('T')
   end
 
   def test_dna_has_no_uracil
-    skip
     assert_equal 0, DNA.new('GATTACA').count('U')
   end
 
   def test_dna_counts_do_not_change_after_counting_uracil
-    skip
     dna = DNA.new('GATTACA')
     dna.count('U')
     expected = {'A' => 3, 'T' => 2, 'C' => 1, 'G' => 1}
@@ -48,28 +42,24 @@ class DNATest < MiniTest::Unit::TestCase
   end
 
   def test_validates_nucleotides
-    skip
     assert_raises ArgumentError do
       DNA.new('GACT').count('X')
     end
   end
 
   def test_validates_dna_not_rna
-    skip
     assert_raises ArgumentError do
       DNA.new('ACGU')
     end
   end
 
   def test_validates_dna
-    skip
     assert_raises ArgumentError do
       DNA.new('John')
     end
   end
 
   def test_counts_all_nucleotides
-    skip
     s = 'AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC'
     dna = DNA.new(s)
     expected = {'A' => 20, 'T' => 21, 'G' => 17, 'C' => 12}
