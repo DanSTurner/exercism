1bb2062b75d06af49a98c3b9a2bbcedee74b36f6
Dan S Turner <turner@nowwithmoredan.com>
2013-11-08 10:58:18 -0800

    Add methods which normalize the input

--- phone-number/phone-number_test.rb
+++ phone-number/phone-number_test.rb
@@ -10,19 +10,16 @@ class PhoneNumberTest < MiniTest::Unit::TestCase
   end
 
   def test_cleans_number_with_dots
-    skip
     number = PhoneNumber.new("123.456.7890").number
     assert_equal "1234567890", number
   end
 
   def test_valid_when_11_digits_and_first_is_1
-    skip
     number = PhoneNumber.new("11234567890").number
     assert_equal "1234567890", number
   end
 
   def test_invalid_with_letters
-    skip
     number = PhoneNumber.new("123-abc-1234").number
     assert_equal "0000000000", number
   end
@@ -40,13 +37,11 @@ class PhoneNumberTest < MiniTest::Unit::TestCase
   end
 
   def test_invalid_when_12_digits_and_first_is_1
-    skip
     number = PhoneNumber.new("112345678901").number
     assert_equal "0000000000", number
   end
 
   def test_area_code
-    skip
     number = PhoneNumber.new("1234567890")
     assert_equal "123", number.area_code
   end
--- phone-number/phone_number.rb
+++ phone-number/phone_number.rb
@@ -3,9 +3,32 @@ def initialize(input_number)
   @input_number = input_number
 end
 
+# refactor using method names to array, call each from the array on the var
 def number
-  @input_number.scan(/\d/).join('')
+  result = zeros_if_not_10_digits(
+    remove_us_country_code(
+      to_all_digits(
+        @input_number
+      )
+    )
+  )
+  return result
 end
 
+def to_all_digits(number)
+  number.scan(/\d/).join('')
+end
+
+def remove_us_country_code(number)
+  if number.length == 11 && number.scan(/./)[0] == "1"
+    number = number.scan(/./)[1..-1].join
+  end
+  return number
+end
+
+def zeros_if_not_10_digits(number)
+  number = "0000000000" if number.length != 10
+  return number
+end
 
 end
