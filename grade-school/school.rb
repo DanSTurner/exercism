7a9228711e6677a863e5677c518da1968042040d
Dan S Turner <turner@nowwithmoredan.com>
2013-11-08 16:35:11 -0800

    Pass all tests

--- grade-school/school.rb
+++ grade-school/school.rb
@@ -2,7 +2,20 @@ class School
 attr_accessor :db
 
 def initialize
-  @db = {}
+  @db = Hash.new{[]}
+end
+
+def add(student_name, grade)
+  @db[grade] <<= student_name   # "@db[grade] =" is obviated by the "=" after the "<<"
+end
+
+def grade(grade_num)
+  @db[grade_num]
+end
+
+def sort
+  @db.each { |key, value| @db[key] = value.sort }
+  @db = Hash[@db.sort]          # sort makes an array, pass it to Hash to rehashify
 end
 
 end
