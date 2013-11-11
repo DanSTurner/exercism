4d893f15d7aee8ee0c46d865022c89e1098b440a
Dan S Turner <turner@nowwithmoredan.com>
2013-11-11 10:52:44 -0800

    Refactor initialize/reset, add logic to reset
    Moved the expressions for initialize to the reset method and added logic
    that calls reset recursively if a different name isn't created.

--- robot-name/robot.rb
+++ robot-name/robot.rb
@@ -2,11 +2,13 @@ class Robot
 attr_reader :name
 
 def initialize
-  @name = (0..1).map { (65 + rand(26)).chr }.join + (0..3).map { rand(9) }.join
+  reset
 end
 
 def reset
-  initialize
+  current = @name
+  @name = (0..1).map { (65 + rand(26)).chr }.join + (0..3).map { rand(9) }.join
+  reset if current == @name
 end
 
 end
