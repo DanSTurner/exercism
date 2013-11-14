60085b606eed1347d93857b6db8a13b712343988
Dan S Turner <turner@nowwithmoredan.com>
2013-11-14 14:41:18 -0800

    Refactor using built in add seconds to Time

--- gigasecond/gigasecond.rb
+++ gigasecond/gigasecond.rb
@@ -3,12 +3,11 @@ require 'time'
 class Gigasecond
   attr_accessor :date
 
-  def initialize(date)
-    @date_in_seconds = date.strftime('%s').to_i
+  def initialize(origin_date)
+    @date = origin_date
   end
 
   def date
-    @date_in_seconds += 1000000000
-    Time.at(@date_in_seconds).utc.to_date
+    (@date.to_time + 1_000_000_000).utc.to_date
   end
 end
