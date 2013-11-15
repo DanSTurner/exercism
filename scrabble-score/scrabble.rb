40b60139bc38ba724c023ef99b562bd35bc64431
Dan S Turner <turner@nowwithmoredan.com>
2013-11-15 10:26:34 -0800

    Add scrabble.rb, pass all tests but one

---
+++ scrabble-score/scrabble.rb
@@ -0,0 +1,18 @@
+class Scrabble
+  attr_accessor :score
+
+  def initialize(word)
+    @word = word
+    @lettervalue = {'a'=>1,'b'=>3,'c'=>3,'d'=>2,'e'=>1,'f'=>4,'g'=>2,'h'=>4,'i'=>1,'j'=>8,'k'=>5,'l'=>1,'m'=>3,'n'=>1,'o'=>1,'p'=>3,'q'=>10,'r'=>1,'s'=>1,'t'=>1,'u'=>1,'v'=>4,'w'=>4,'x'=>8,'y'=>4,'z'=>10}
+  end
+
+  def score
+    clean(@word).scan(/./).inject(0) do |score, letter|
+      score + @lettervalue[letter]
+    end
+  end
+
+  def clean(dirty)
+    dirty.nil? ? "" : dirty.downcase.gsub(/[^a-z]/, '')
+  end
+end
