254c8ff8110d2e719d10f3b9c92e9e7d53a62606
Dan S Turner <turner@nowwithmoredan.com>
2013-12-05 09:39:11 -0800

    Still much too slow

--- prime-factors/prime_factors.rb
+++ prime-factors/prime_factors.rb
@@ -1,39 +1,27 @@
-require 'mathn'
-
 class PrimeFactors
-  # def self.for(num)
-  #   @set = Array.new
-  #   temp = num
-  #   (2..num).each do |factor|
-  #     if num / factor == num.to_f / factor
-  #       @set.push(factor/temp)
-  #       temp = num / factor
-  #     end
-  #   end
-  #   return @set
-  # end
-
   def self.for(num)
     @num = num
     @set = Array.new
-    (2..@num).each { |candidate| self.recurse(candidate) }
+    (2..@num).find do |candidate|
+      self.recurse(candidate) if self.is_eligible?(candidate)
+    end
     return @set
   end
 
   def self.recurse(candidate)
-    if self.eligible?(candidate)
+
+    if self.is_eligible?(candidate)
       @num = @num / candidate
       @set.push(candidate)
       self.recurse(candidate)
     end
   end
 
-  def self.eligible?(candidate)
-    candidate <= @num && @num % candidate == 0 && is_prime?(candidate)
+  def self.is_eligible?(candidate)
+    candidate <= @num && is_factor?(candidate)
   end
 
-  def self.is_prime?(candidate)
-    Prime.prime?(candidate)
+  def self.is_factor?(candidate)
+    @num % candidate == 0
   end
-
 end
